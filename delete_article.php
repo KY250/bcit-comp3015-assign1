a5cfa7c50f431799d88ff7c1e73629093b61f49a
KY250 <72047105+KY250@users.noreply.github.com>
2022-09-30 13:02:26 -0700

    Update delete_article.php

--- delete_article.php
+++ delete_article.php
@@ -4,6 +4,10 @@ require_once 'src/ArticleRepository.php'; // Hint. You'll need this.
 
 // Accept the ID as a query parameter of an HTTP GET request
 // then, call a function such as ArticleRepository@deleteArticleById($id)
+
+$articleRepository = new ArticleRepository("articles.json");
+$articleRepository->deleteArticleById($id);
+
 // After that, redirect the user back to the index page (use the header function for this)
 ?>
 
@@ -12,10 +16,5 @@ require_once 'src/ArticleRepository.php'; // Hint. You'll need this.
 <?php require_once 'layout/header.php' ?>
 <body>
 <?php require_once 'layout/navigation.php' ?>
-<form>
-  <input type="text" id="fname" name="fname"><br> <br>
-  <input type="text" id="lname" name="lname"><br> <br>
-  <input type="submit" value="Submit">
-</form>
 </body>
 </html>
