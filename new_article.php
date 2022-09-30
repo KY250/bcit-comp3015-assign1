94e4886980f853c612ff89fe6ca29ab4e76b2e20
KY250 <72047105+KY250@users.noreply.github.com>
2022-09-30 13:00:56 -0700

    Update new_article.php

--- new_article.php
+++ new_article.php
@@ -3,6 +3,14 @@
 require_once 'src/ArticleRepository.php';
 require_once 'src/Models/Article.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $link = $_POST['link']; // the link name here will depend on what name attribute the new input has
+    $articleRepository->saveNewArticle($title, $link);
+}
+
+$articleRepository = new ArticleRepository('articles.json');
+$articles = $articleRepository->getAllArticles(); 
 // We can create a <form> here that will make an HTTP POST request to the server
 // in order to save a new article to file.
 ?>
@@ -12,9 +20,24 @@ require_once 'src/Models/Article.php';
 <?php require_once 'layout/header.php' ?>
 <body>
 <?php require_once 'layout/navigation.php' ?>
-<div class="container mx-auto mt-10">
-    <h1 class="font-bold">This application provides examples of using forms to accept input.</h1>
-    <p>This is the index page (index.php).</p>
-</div>
+    <form action="new_article.php" method="post">
+    <label>Title:
+    <input class="gv-input" type="text" name="title"><br> <br>
+    </label>
+    
+    <label>
+    <input type="submit" value="Submit">
+    </label>
+    </form>
 </body>
 </html>
+
+<style>
+    .gv-input {
+        border:1px solid;
+    }
+	form {
+		text-align:center;
+        margin:70px;
+	}
+</style>
